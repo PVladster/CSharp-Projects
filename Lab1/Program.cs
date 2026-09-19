88787ab12ac32238faf9821842134a541c519c67
Panchenko Vlada <pvlada322@gmail.com>
2026-09-19 15:38:37 +0300

    Add Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           double a = GetSideFromUser("a");
+           double b = GetSideFromUser("b");
+           double c = GetSideFromUser("c");
+
+           Triangle triangle = new Triangle();
+           if(triangle.AreSidesPositive(a,b,c) && triangle.IsTriangleValid(a,b,c))
+            {
+                double perimeter = triangle.CalculatePerimeter(a,b,c);
+                double area = triangle.CalculateArea(a,b,c);
+                string type = triangle.DetermineTriangleType(a,b,c);
+
+                Console.WriteLine($"Perimeter: {perimeter}");
+                Console.WriteLine($"Area: {area}");
+                Console.WriteLine($"Type of triangle: {type}");
+            }
+            else
+            {
+                Console.WriteLine("Error: sides not possitive or triangle not be avalable");
+            }
+        }
+        static double GetSideFromUser(string sideName)
+        {
+           double result;
+            do
+            {
+                Console.Write($"Enter side {sideName}: ");
+            } while(!double.TryParse(Console.ReadLine(), out result));
+            return result;
+        }
+    }
+}
